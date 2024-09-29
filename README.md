7da5e44b3881e0e52f24f12c8cffd8d123224926
Wesley Reisz <wes@wesleyreisz.com>
2024-09-29 07:23:49 -0400

    added logging

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
+#Running Functions
 Simple hello world target for function
 code.
+
+reference:
+* https://cloud.google.com/functions/docs/create-deploy-http-java
+
+### run locally
+`mvn function:run -Drun.functionTarget=functions.HelloWorld`
+
+### deploy to gCloud
+`export REGION=us-east2`
+`gcloud functions deploy java-http-function \
+--gen2 \
+--entry-point=functions.HelloWorld \
+--runtime=java17 \
+--region=$REGION \
+--source=. \
+--trigger-http \
+--allow-unauthenticated`
--- src/main/java/functions/HelloWorld.java
+++ src/main/java/functions/HelloWorld.java
@@ -4,16 +4,22 @@ package functions;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+
+import java.util.logging.Logger;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class HelloWorld implements HttpFunction {
-  // Simple function to return "Hello World"
+  private static final Logger logger = Logger.getLogger(HelloWorld.class.getName());
+
   @Override
   public void service(HttpRequest request, HttpResponse response)
       throws IOException {
+    logger.info("Running the HelloWorld service.");
     BufferedWriter writer = response.getWriter();
     writer.write("Hello World!\n\n");
+    writer.newLine();
     writer.write(System.getenv().toString());
   }
 }
